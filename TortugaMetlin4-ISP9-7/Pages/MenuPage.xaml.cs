2e26774f8ecc3c94d5ef54340d6c05b31b534690
Sergey Panchenko <forelock10@gmail.com>
2022-11-11 14:26:56 +0300

    4

--- TortugaMetlin4-ISP9-7/Pages/MenuPage.xaml.cs
+++ TortugaMetlin4-ISP9-7/Pages/MenuPage.xaml.cs
@@ -20,6 +20,7 @@ namespace TortugaMetlin4_ISP9_7.Pages
     /// </summary>
     public partial class MenuPage : Page
     {
+        int idCat;
         public MenuPage()
         {
             InitializeComponent();
@@ -28,27 +29,56 @@ namespace TortugaMetlin4_ISP9_7.Pages
         private void btnBar_Click(object sender, RoutedEventArgs e)
         {
             tbBar.TextDecorations = TextDecorations.Underline;
-            tbHotDishes.TextDecorations = TextDecorations.Underline;
+            tbHotDishes.TextDecorations = null;
+            tbDesserts.TextDecorations = null;
+            tbSalads.TextDecorations = null;
+            tbSnacks.TextDecorations = null;
+            idCat = 1;
+            lvMenu.ItemsSource = ClassHelper.AppData.Context.Dish.Where(i => i.IDCategory == idCat).ToList();
         }
 
         private void btnHotDishes_Click(object sender, RoutedEventArgs e)
         {
+            tbBar.TextDecorations = null;
             tbHotDishes.TextDecorations = TextDecorations.Underline;
+            tbDesserts.TextDecorations = null;
+            tbSalads.TextDecorations = null;
+            tbSnacks.TextDecorations = null;
+            idCat = 3;
+            lvMenu.ItemsSource = ClassHelper.AppData.Context.Dish.Where(i => i.IDCategory == idCat).ToList();
         }
 
         private void btnDesserts_Click(object sender, RoutedEventArgs e)
         {
+            tbBar.TextDecorations = null;
+            tbHotDishes.TextDecorations = null;
             tbDesserts.TextDecorations = TextDecorations.Underline;
+            tbSalads.TextDecorations = null;
+            tbSnacks.TextDecorations = null;
+            idCat = 2;
+            lvMenu.ItemsSource = ClassHelper.AppData.Context.Dish.Where(i => i.IDCategory == idCat).ToList();
         }
 
         private void btnSalads_Click(object sender, RoutedEventArgs e)
         {
+            tbBar.TextDecorations = null;
+            tbHotDishes.TextDecorations = null;
+            tbDesserts.TextDecorations = null;
             tbSalads.TextDecorations = TextDecorations.Underline;
+            tbSnacks.TextDecorations = null;
+            idCat = 4;
+            lvMenu.ItemsSource = ClassHelper.AppData.Context.Dish.Where(i => i.IDCategory == idCat).ToList();
         }
 
         private void btnSnacks_Click(object sender, RoutedEventArgs e)
         {
+            tbBar.TextDecorations = null;
+            tbHotDishes.TextDecorations = null;
+            tbDesserts.TextDecorations = null;
+            tbSalads.TextDecorations = null;
             tbSnacks.TextDecorations = TextDecorations.Underline;
+            idCat = 5;
+            lvMenu.ItemsSource = ClassHelper.AppData.Context.Dish.Where(i => i.IDCategory == idCat).ToList();
         }
     }
 }
